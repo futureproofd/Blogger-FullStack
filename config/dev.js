f5fabeef328b3f56c698bac44f84adc86bb6b28f
Marcus Plienegger <futureproofd@users.noreply.github.com>
2019-06-20 10:24:35 -0400

    working without Redis

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI:
+    'mongodb://dbUser:QReBMtI2oElQQKhV@blogproject-shard-00-00-yy9l7.mongodb.net:27017,blogproject-shard-00-01-yy9l7.mongodb.net:27017,blogproject-shard-00-02-yy9l7.mongodb.net:27017/test?ssl=true&replicaSet=BlogProject-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123',
+};
---
+++ middlewares/requireLogin.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
+  if (!req.user) {
+    return res.status(401).send({ error: 'You must log in!' });
+  }
+
+  next();
+};
